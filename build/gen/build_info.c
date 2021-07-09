ac100b6b37b5f593fe43273e88e3f159d47a6fa6
goodspeed1986 <goodspeed1986@gmail.com>
2021-07-09 23:45:18 +0300

    1.4 with build

--- build/gen/build_info.c
+++ build/gen/build_info.c
@@ -1,4 +1,4 @@
 /* Auto-generated, do not edit. */
-const char *build_id = "20210708-174453";
-const char *build_timestamp = "2021-07-08T17:44:53Z";
-const char *build_version = "1.3";
+const char *build_id = "20210709-204444";
+const char *build_timestamp = "2021-07-09T20:44:44Z";
+const char *build_version = "1.4";
